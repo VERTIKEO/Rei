21296d7f1a97577f9284591a79a65514b26f244d
Elly <elly.ljungman.3489@student.uu.se>
2020-04-17 16:02:24 +0200

    Setup demo-room
    Updated meshes such as SM_WallCornerIn and SM_WallCornerOut as well as the pivot point on most meshes to be easier to place. Created colliders and prefabs for Floor3x3, Wall3x3, WallCornerIn, WallCornerOut, WallDoorframe, Bureau, Couch, Player, Camera. Replaced old assets with the new prefabs. Imported scripts from Christoffer for player controller and camera controller so that player and camera now can move in demo room. Replaced skybox to just a dark color and dimmed the directional lights not to be the main light source but to still give ambient light. Added some point lights randomly in the level just to represent what it could look like if we lit with wall mounted candles later on. placed a bright pointlight on as a child to player as well, as if the player keeps a candle with them. Think that's it for now. And setup the feature branch demo_room for these changes.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    CharacterController characterController;
+    public float speed = 5.0f;
+     private Vector3 moveDirection = Vector3.zero;
+    public float gravity = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"))*speed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            float angle = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
+        }
+
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
