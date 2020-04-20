d5c3c04bc337d6496d0f8f1c9f3fb7e640df762f
Christoffer Öberg <christofferoberg95@gmail.com>
2020-04-20 12:05:10 +0200

    Implemented Inventory System

---
+++ Assets/Scripts/activator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activator : MonoBehaviour
+{
+
+    bool playerInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Debug.Log("Interacted");
+            }
+        }
+    }
+}
