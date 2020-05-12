0610b761dd2627e6d25c1f9ecabd9a11ea4f53d4
James Kerber <kerber.jg@gmail.com>
2020-05-12 12:56:58 +0200

    Add light/reflection probes to Library and Entrance

---
+++ Assets/ReflectionChild.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ReflectionChild : MonoBehaviour
+{
+    // TODO: on PostProcessScene
+    void Awake()
+    {
+        BoxCollider c;
+        if(transform.parent.gameObject.TryGetComponent<BoxCollider>(out c)) {
+            ReflectionProbe r = GetComponent<ReflectionProbe>(); 
+            r.size = c.size;
+            r.center = c.center - transform.localPosition;
+        }
+
+        this.enabled = false;
+    }
+}
