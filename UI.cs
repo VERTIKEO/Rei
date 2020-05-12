0c3a1e6c0e9ef89a3fdc91d2636d19388dff2071
HM199 <49558880+HM199@users.noreply.github.com>
2020-05-12 12:27:54 +0200

    Script for UI

---
+++ UI.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] private Image UserInterface;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UserInterface.enabled = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UserInterface.enabled = false;
+        }
+    }
+
+
+
+
+}
