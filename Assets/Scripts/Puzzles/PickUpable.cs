f6a4cf7a4613bbc1ec97278b41fefee7c42dd35a
Christoffer Öberg <christofferoberg95@gmail.com>
2020-05-29 21:48:54 +0200

    Everything should work now

--- Assets/Scripts/Puzzles/PickUpable.cs
+++ Assets/Scripts/Puzzles/PickUpable.cs
@@ -5,33 +5,47 @@ using UnityEngine.UI;
 
 public class PickUpable : MonoBehaviour
 {
-
-    GameObject textBox;
+    bool playerClose = false;
+    GameObject pickUpKey;
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        textBox = GameObject.Find("Intro");
+        pickUpKey = GameObject.Find("KeyFlowchart");
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") && playerClose == true)
+        {
+            pickUpKey.SetActive(true);
+
+        }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                
-                PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
-                playerController.inventory.Add(gameObject.name);
-                textBox.SendMessage("PickedUpKey");
-                
-                gameObject.SetActive(false);
-            }
+            playerClose = true;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerClose = false;
+        }
+    }
+
+    public void PickUp()
+    {
+        PlayerController playerController = player.gameObject.GetComponent<PlayerController>();
+        playerController.inventory.Add(gameObject.name);
+
+        gameObject.SetActive(false);
     }
 }
