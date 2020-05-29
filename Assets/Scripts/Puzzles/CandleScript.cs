e7fb1408facba156c19054f6d6e7a3896911e891
Christoffer Öberg <christofferoberg95@gmail.com>
2020-05-29 11:35:04 +0200

    Slight bugfix

--- Assets/Scripts/Puzzles/CandleScript.cs
+++ Assets/Scripts/Puzzles/CandleScript.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class CandleScript : MonoBehaviour
 {
+    Book bookScript;
+    public GameObject book;
+
+    private void Start()
+    {
+        book = GameObject.Find("BookCube");
+        bookScript = book.GetComponent<Book>();
+    }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (Input.GetButtonDown("Fire1"))
+            if (Input.GetButtonDown("Fire1") && bookScript.puzzleStarted == true)
             {
                 PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
                 
